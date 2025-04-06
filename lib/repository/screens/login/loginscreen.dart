364cd12a43f672a1579d04aa7bd75ed72551f3b5
Vikas Gautam <gautamvikas754@gmail.com>
2025-04-06 13:44:13 +0530

    loginScreen Completed

--- lib/repository/screens/login/loginscreen.dart
+++ lib/repository/screens/login/loginscreen.dart
@@ -1,10 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/repository/screens/bottomnav/bottomnavscreen.dart';
+import 'package:flutter_application_2/repository/widgtes/uihelper.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Login Screen')));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            UiHelper.customImage(img: "Blinkit Onboarding Screen.png"),
+            SizedBox(height: 30),
+            UiHelper.customImage(img: "image 10.png"),
+            SizedBox(height: 20),
+            UiHelper.customText(
+              text: "India's last minute app",
+              color: Colors.black,
+              fontweight: FontWeight.bold,
+              fontsize: 20,
+              fontfamily: "bold",
+            ),
+            SizedBox(height: 20),
+            Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                height: 200,
+                width: 350,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color(0XFFFFFFFF),
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(height: 20),
+                    UiHelper.customText(
+                      text: "Vikas",
+                      color: Color(0XFF000000),
+                      fontweight: FontWeight.w500,
+                      fontsize: 14,
+                    ),
+                    SizedBox(height: 5),
+                    UiHelper.customText(
+                      text: "73041XXXXX",
+                      color: Color(0XFF9C9C9C),
+                      fontweight: FontWeight.bold,
+                      fontsize: 14,
+                      fontfamily: "bold",
+                    ),
+                    SizedBox(height: 20),
+                    SizedBox(
+                      height: 48,
+                      width: 295,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => BottomNavScreen(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0XFFE23744),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            UiHelper.customText(
+                              text: "Login  with",
+                              color: Color(0XFFFFFFFF),
+                              fontweight: FontWeight.bold,
+                              fontsize: 14,
+                              fontfamily: "bold",
+                            ),
+                            SizedBox(width: 5),
+                            UiHelper.customImage(img: "image 9.png"),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    UiHelper.customText(
+                      text:
+                          "Access your saved addresses from Zomato automatically!",
+                      color: Color(0XFF9C9C9C),
+                      fontweight: FontWeight.normal,
+                      fontsize: 10,
+                    ),
+                    SizedBox(height: 15),
+                    UiHelper.customText(
+                      text: "or login with phone number",
+                      color: Color(0XFF269237),
+                      fontweight: FontWeight.normal,
+                      fontsize: 14,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
